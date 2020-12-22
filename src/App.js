f7855252c818948afaf888b33349a138797e86b8
Bluette1 <marylene.sawyer@gmail.com>
2020-12-22 17:34:14 +0200

    Add logo, app header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import React from 'react';
 import BooksForm from './containers/BooksForm';
 import BooksList from './containers/BooksList';
+import logo from './logo.png';
 
 export default function App() {
   return (
     <div>
-      <h1 className="bookstore-heading">Bookstoria</h1>
+      <ul className="bookstore-heading">
+        <li>
+          <img src={logo} className="app-logo" alt="logo" />
+          Bookstoria
+        </li>
+        <li>BOOKS</li>
+        <li>CATEGORIES</li>
+      </ul>
       <BooksList />
       <BooksForm />
     </div>
