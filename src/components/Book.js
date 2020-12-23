9c2826598acb50473e405c01470b84a0ea847ff8
Bluette1 <marylene.sawyer@gmail.com>
2020-12-23 15:27:49 +0200

    Add the recommended styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,22 +4,40 @@ import PropTypes from 'prop-types';
 import { removeBook } from '../actions/index';
 
 const Book = ({ book, handleRemoveBook }) => {
-  const { id, title, category } = book;
+  const { title, category } = book;
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        <button
-          type="button"
-          className="submit-delete"
-          onClick={() => handleRemoveBook(book)}
-        >
-          Remove Book
-        </button>
-      </td>
-    </tr>
+    // <tr>
+    //   <td>{id}</td>
+    //   <td>{title}</td>
+    //   <td>{category}</td>
+    //   <td>
+    //     <button
+    //       type="button"
+    //       className="submit-delete"
+    //       onClick={() => handleRemoveBook(book)}
+    //     >
+    //       Remove Book
+    //     </button>
+    //   </td>
+    // </tr>
+    <div className="row">
+      <div>
+        <p className="category">{category}</p>
+        <h4 className="title">{title}</h4>
+        <ul className="update">
+          <li>Comments</li>
+          <li aria="hidden" role="presentation" onClick={() => handleRemoveBook(book)}>Remove</li>
+          <li>Edit</li>
+        </ul>
+      </div>
+      <div>
+        <div className="percentage" />
+      </div>
+      <div>
+        <h4 className="current-chapter">CURRENT CHAPTER</h4>
+        <p className="current-lesson">Introduction</p>
+      </div>
+    </div>
   );
 };
 
