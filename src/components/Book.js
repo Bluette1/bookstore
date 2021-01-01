8cb29f3e7a1993d7f1e7c6e64c53eed1b55dedf3
Bluette1 <marylene.sawyer@gmail.com>
2021-01-01 18:34:09 +0200

    Handle onClick `Edit` link
    - Modifications on the update form

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,9 +11,8 @@ class Book extends React.Component {
   constructor(props) {
     super(props);
     const { props: { book } } = this;
-    const { totalPages, pagesRead, currentChapter } = book;
     this.state = {
-      totalPages, pagesRead, showForm: false, currentChapter,
+      showProgressForm: false, showUpdateForm: false, ...book,
     };
     this.handleRemoveBook = this.handleRemoveBook.bind(this);
     this.handleChangePagesRead = this.handleChangePagesRead.bind(this);
@@ -21,7 +20,9 @@ class Book extends React.Component {
     this.handleChangeCurrentChapter = this.handleChangeCurrentChapter.bind(this);
     this.handleUpdateBook = this.handleUpdateBook.bind(this);
     this.hideUpdateForm = this.hideUpdateForm.bind(this);
+    this.hideUpdateProgressForm = this.hideUpdateProgressForm.bind(this);
     this.showUpdateForm = this.showUpdateForm.bind(this);
+    this.showUpdateProgressForm = this.showUpdateProgressForm.bind(this);
   }
 
   handleChangePagesRead = pagesRead => {
@@ -36,6 +37,18 @@ class Book extends React.Component {
     this.setState({ currentChapter });
   };
 
+  handleChangeTitle = title => {
+    this.setState({ title });
+  };
+
+  handleChangeCategory = category => {
+    this.setState({ category });
+  };
+
+  handleChangeAuthor = author => {
+    this.setState({ author });
+  };
+
   handleRemoveBook() {
     const { props: { book } } = this;
     axios.delete(`${httpProtocol}://${host}:${port}/books/${book.id}`)
@@ -48,29 +61,40 @@ class Book extends React.Component {
   handleUpdateBook() {
     const { props: { book } } = this;
     const {
-      pagesRead, totalPages, currentChapter,
+      pagesRead, totalPages, currentChapter, title, category, author,
     } = this.state;
-    axios.put(`${httpProtocol}://${host}:${port}/books/${book.id}`, { pagesRead, totalPages, currentChapter })
+    axios.put(`${httpProtocol}://${host}:${port}/books/${book.id}`, {
+      pagesRead, totalPages, currentChapter, title, category, author,
+    })
       .then(response => {
         const { props: { updateBook } } = this;
         updateBook(response.data);
       });
   }
 
+  showUpdateProgressForm() {
+    this.setState({ showProgressForm: true });
+  }
+
+  hideUpdateProgressForm() {
+    this.setState({ showProgressForm: false });
+    this.handleUpdateBook();
+  }
+
   showUpdateForm() {
-    this.setState({ showForm: true });
+    this.setState({ showUpdateForm: true });
   }
 
   hideUpdateForm() {
-    this.setState({ showForm: false });
+    this.setState({ showUpdateForm: false });
     this.handleUpdateBook();
   }
 
   render() {
-    const { props: { book } } = this;
-    const { title, category } = book;
     const {
-      pagesRead, totalPages, showForm, currentChapter,
+      pagesRead,
+      totalPages,
+      showProgressForm, showUpdateForm, currentChapter, title, category, author,
     } = this.state;
     const value = (pagesRead / totalPages).toFixed(2);
     return (
@@ -78,10 +102,11 @@ class Book extends React.Component {
         <div className="title-category">
           <p className="category">{category}</p>
           <h4 className="title">{title}</h4>
+          <p className="author">{author}</p>
           <ul className="comments-section">
             <li>Comments</li>
             <li aria-hidden="true" role="presentation" onClick={() => this.handleRemoveBook()}>Remove</li>
-            <li>Edit</li>
+            <li role="presentation" onClick={() => this.showUpdateForm()}>Edit</li>
           </ul>
         </div>
         <div className="progress">
@@ -98,25 +123,59 @@ class Book extends React.Component {
         <div>
           <h4 className="current-chapter">CURRENT CHAPTER</h4>
           <p className="current-lesson">{currentChapter}</p>
-          <button type="button" className="update-progress-btn" onClick={() => this.showUpdateForm()}>
+          <button type="button" className="update-progress-btn" onClick={() => this.showUpdateProgressForm()}>
             <span className="update-progress">UPDATE PROGRESS</span>
           </button>
-          {showForm ? (
+          {showProgressForm ? (
+            <div className="form-div">
+              <div className="popup-form">
+                <form id="form">
+                  <i className="fa fa-times-circle-o fa-lg" aria-hidden="true" onClick={() => this.hideUpdateProgressForm()} />
+                  <h2>Update Reading Progress</h2>
+                  <p htmlFor="book-title" className="book-title">
+                    {title}
+                    <br />
+                  </p>
+                  <label htmlFor="pages-read">
+                    Pages read:
+                    <input id="pages-read" name="pages-read" placeholder={pagesRead} type="number" onChange={e => this.handleChangePagesRead(e.target.value)} />
+                  </label>
+                  <label htmlFor="total-pages">
+                    Total pages:
+                    <input id="total-pages" name="total-pages" placeholder={totalPages} type="number" onChange={e => this.handleChangeTotalPages(e.target.value)} />
+                  </label>
+                  <label htmlFor="current-chapter">
+                    Current chapter:
+                    <input id="current-chapter" name="current-chapter" placeholder={currentChapter} type="text" onChange={e => this.handleChangeCurrentChapter(e.target.value)} />
+                  </label>
+                  <button type="submit" onClick={() => this.hideUpdateProgressForm()}>OK</button>
+                </form>
+              </div>
+            </div>
+          ) : null }
+          {showUpdateForm ? (
             <div className="form-div">
               <div className="popup-form">
                 <form id="form">
                   <i className="fa fa-times-circle-o fa-lg" aria-hidden="true" onClick={() => this.hideUpdateForm()} />
-                  <h2>Update Reading Progress</h2>
-                  <label htmlFor="book-title">{title}</label>
-                  <input id="pages-read" name="pages-read" placeholder="pages read" type="number" onChange={e => this.handleChangePagesRead(e.target.value)} />
-                  <input id="total-pages" name="total-pages" placeholder="total-pages" type="number" onChange={e => this.handleChangeTotalPages(e.target.value)} />
-                  <input id="current-chapter" name="current-chapter" placeholder="current chapter" type="text" onChange={e => this.handleChangeCurrentChapter(e.target.value)} />
+                  <h2>Update book</h2>
+                  <label htmlFor="book-title">
+                    Title:
+                    <input id="title" name="title" placeholder={title} type="text" onChange={e => this.handleChangeTitle(e.target.value)} />
+                  </label>
+                  <label htmlFor="book-category">
+                    Category:
+                    <input id="category" name="category" placeholder={category} type="text" onChange={e => this.handleChangeCategory(e.target.value)} />
+                  </label>
+                  <label htmlFor="book-author">
+                    Author:
+                    <input id="book-author" name="author" placeholder={author} type="text" onChange={e => this.handleChangeAuthor(e.target.value)} />
+                  </label>
                   <button type="submit" onClick={() => this.hideUpdateForm()}>OK</button>
                 </form>
               </div>
             </div>
           ) : null }
-
         </div>
       </div>
     );
